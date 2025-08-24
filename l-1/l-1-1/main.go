8d34909a8de5f346499126da284b6ab2a9112b9d
Emil Yay <keyt.isac@yandex.ru>
2025-08-24 05:25:28 +0300

    l-1-1 complete

---
+++ l-1/l-1-1/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type Human struct {
+	name string
+	age  int
+	sex  string
+}
+
+func (h *Human) Name() string {
+	return h.name
+}
+
+func (h *Human) Age() int {
+	return h.age
+}
+
+func (h *Human) Sex() string {
+	return h.sex
+}
+
+func (h *Human) SetAge(age int) {
+	h.age = age
+}
+
+func (h *Human) SetSex(s string) {
+	h.sex = s
+}
+
+type Action struct {
+	Human
+}
+
+func main() {
+	a := Action{
+		Human: Human{name: "Sasha", age: 18, sex: "male"},
+	}
+
+	fmt.Println("name:", a.Name())
+	fmt.Println("age:", a.Age())
+
+	a.SetAge(30)
+	a.SetSex("female")
+
+	fmt.Println("updated age:", a.Age())
+	fmt.Println("updated sex:", a.Sex())
+}
