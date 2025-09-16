4dfa9a8942fe926310c18a60c8c92857d6166b50
Emil Yay <keyt.isac@yandex.ru>
2025-09-16 03:10:57 +0300

    l-1-20 complete

--- l-1/l-1-19/main.go
+++ l-1/l-1-19/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 //Разработать программу, которая переворачивает подаваемую на вход строку.
@@ -13,8 +14,9 @@ func main() {
 	var str string
 
 	reader := bufio.NewReader(os.Stdin)
-
 	str, _ = reader.ReadString('\n')
+	str = strings.TrimSpace(str)
+	
 	runes := []rune(str)
 	for i := len(runes) - 1; i >= 0; i-- {
 		fmt.Print(string(runes[i]))
